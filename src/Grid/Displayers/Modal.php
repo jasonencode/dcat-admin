6a6c17d77e225877971350c74dee1fda83c06b46
Jason <chenjxlg@163.com>
2023-08-31 19:12:28 +0800

    title

--- src/Grid/Displayers/Modal.php
+++ src/Grid/Displayers/Modal.php
@@ -9,7 +9,7 @@ use Dcat\Admin\Widgets\Modal as WidgetModal;
 
 class Modal extends AbstractDisplayer
 {
-    protected string $title;
+    protected string|null $title = null;
 
     protected bool $xl = false;
 
