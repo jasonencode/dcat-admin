5ba4775bd3b4c667cd926f3f0103ef8252495be7
Jason.Chen <chenjxlg@163.com>
2024-01-12 11:12:36 +0800

    sidebar 缩进，user返回数据修正

--- src/Admin.php
+++ src/Admin.php
@@ -139,9 +139,9 @@ class Admin
     /**
      * 获取登录用户模型.
      *
-     * @return Model|Authenticatable
+     * @return Model|Authenticatable|null
      */
-    public static function user(): Authenticatable|Model
+    public static function user(): Authenticatable|Model|null
     {
         return static::guard()->user();
     }
